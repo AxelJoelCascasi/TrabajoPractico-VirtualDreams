8930d5bed7a666e08b6c7174181f242485c66758
acascasi13 <48626905+acascasi13@users.noreply.github.com>
2021-03-13 12:10:35 -0300

    Add files via upload

---
+++ Ejercicio4/mostrarPersonas.js
@@ -0,0 +1,7 @@
+const request = require('request-promise');
+
+request ('https://reclutamiento-14cf7.firebaseio.com/personas.json',function(error,response,body){
+    console.log(JSON.parse(body));
+    console.log('statusCode:',response.statusCode);
+})
+
